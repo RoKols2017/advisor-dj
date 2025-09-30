353cf8b0353c4bc1ac9fc52dcd3ef3625f124201
RoKols2017 <rokols2017@gmail.com>
2025-09-30 11:58:48 +0300

    chore: remove legacy flask/ directory not used by Django app

--- flask/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Импорт моделей для регистрации
-from .building import Building
-from .computer import Computer
-from .department import Department
-from .port import Port
-from .print_event import PrintEvent
-from .printer import Printer
-from .printer_model import PrinterModel
-from .user import User

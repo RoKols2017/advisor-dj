e05dd7c91f90752bb64f30b296e381fcb6ac0bff
Roman <roman@example.com>
2025-12-27 09:59:37 +0300

    Исправлено отображение отделов в фильтрах: показывается только название
    - Добавлено кастомное поле DepartmentChoiceField для фильтра отделов
    - В фильтре 'События печати' теперь показывается только название отдела без кода
    - Обновлена документация: добавлено упоминание об исправлении фильтров

--- printing/filters.py
+++ printing/filters.py
@@ -4,6 +4,12 @@ from django import forms
 from .models import Computer, Department, Printer, PrintEvent
 
 
+class DepartmentChoiceField(forms.ModelChoiceField):
+    """Кастомное поле для выбора отдела с отображением только названия."""
+    def label_from_instance(self, obj):
+        return obj.name
+
+
 class PrintEventFilter(django_filters.FilterSet):
     timestamp = django_filters.DateFromToRangeFilter(
         widget=django_filters.widgets.RangeWidget(attrs={'class': 'form-control', 'type': 'date'})
@@ -13,9 +19,10 @@ class PrintEventFilter(django_filters.FilterSet):
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     user__department = django_filters.ModelChoiceFilter(
-        queryset=Department.objects.all(),
+        queryset=Department.objects.all().order_by('name'),
         label='Отдел',
-        widget=forms.Select(attrs={'class': 'form-control'})
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        field_class=DepartmentChoiceField
     )
     printer = django_filters.ModelChoiceFilter(
         queryset=Printer.objects.all(),
